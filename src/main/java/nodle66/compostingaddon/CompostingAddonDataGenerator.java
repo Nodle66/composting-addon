5f3c1dc12ba8940ac22e6af563c891e3b5739679
Nodle66 <nodle66@gmail.com>
2024-02-24 14:33:06 +0200

    initial commit
    Initial commit

---
+++ src/main/java/nodle66/compostingaddon/CompostingAddonDataGenerator.java
@@ -0,0 +1,11 @@
+package nodle66.compostingaddon;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class CompostingAddonDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
